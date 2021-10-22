0e6d469410ed2c74e8c01ea35a24e0e1e5cbcd08
Jonathan Perraudin <jperraud@student.ethz.ch>
2021-10-22 18:11:37 +0200

    write to log file

--- python_scrapper/python_scrapper/__main__.py
+++ python_scrapper/python_scrapper/__main__.py
@@ -1,8 +1,14 @@
-from helpers import get_warnings
+from helpers import get_warnings, get_current_time
+
 
 def main():
     url = "https://zh.stwarn.ch/"
+    filename = 'sturmwarnung_log.txt'
     warnings = get_warnings(url)
+
+    with open(filename, 'a') as file:
+        file.write(get_current_time() + '\n')
+        file.write('\n'.join('%s %s' % x for x in warnings) + '\n')
 
 
 if __name__ == '__main__':
